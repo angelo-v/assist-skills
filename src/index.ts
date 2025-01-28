f53487c06679efbe0471a1a6fdbfa38c2705e405
Angelo Veltens <inbox+github.com@veltens.org>
2025-01-28 19:59:53 +0100

    allow port mapping from outside docker

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = build({
     }
 })
 
-app.listen({port: 3000}, (err) => {
+app.listen({port: 3000, host: "0.0.0.0"}, (err) => {
     if (err) {
         app.log.error(err)
         process.exit(1)
